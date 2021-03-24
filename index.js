28514a322e71c32c1080db92644e4150d264c806
Yadnesh Khode <yadneshkhode@gmail.com>
2021-03-24 16:18:40 +0530

    fixed mailing functionality

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ const nodemailer = require("nodemailer");
 // let bodyParser = require('body-parser')
 let entries = require("./entries");
 const dotenv = require("dotenv");
-
 dotenv.config();
 let app = express();
 app.use(morgan("dev"));
@@ -16,29 +15,13 @@ app.use(express.json());
 app.get("/", (_, res) => {
   res.send("Hello From Backend");
 });
+
 app.get("/entries", (_, res) => {
   const hiringentity = entries.getEntries();
   res.json(hiringentity);
 });
 
 app.post("/entries", (req, res) => {
-  console.log("Data received - ", req.body);
-  entries.putEntry(req.body);
-  sendMailToMe();
-  const hiringentity = entries.getEntries();
-  console.log("Stored successfully!");
-  res.send(hiringentity,200);
-});
-
-const sendMailToMe = () => {
-  const hiringentity = entries.getEntries();
-  console.log(hiringentity);
-  const name = hiringentity.name;
-  console.log(name);
-  const email = hiringentity.email;
-  console.log(email);
-  const message = hiringentity.message;
-  console.log(message);
   let transporter = nodemailer.createTransport({
     service: "gmail",
     auth: {
@@ -51,11 +34,13 @@ const sendMailToMe = () => {
   transporter.sendMail({
     from: "hiringthroughportfolio001@gmail.com", // sender address
     to: "yadneshkhode@gmail.com,yadneshk71@gmail.com", //list of receivers
-    subject: `HIRING THROUGH YOUR PORTFOLIO - ${name}`, // Subject line
+    subject: `HIRING THROUGH YOUR PORTFOLIO - ${req.body.name}`, // Subject line
     text: message, // plain text body
-    html: `<b>Hello my name is ${name} and Email id to contact me is ${email}</b> and my message = ${message}`, // html body
+    html: `<b>Hello my name is ${req.body.name} and Email id to contact me is ${req.body.email}</b> and my message = ${req.body.message}`, // html body
   });
-};
+
+  res.status(200).send(req.body);
+});
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
